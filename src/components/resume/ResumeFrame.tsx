ff94c70b5e127ababeb6972929bc11c5992cf09b
Alan Varghese Paul <alanvarghesepaul29@gmail.com>
2023-08-07 22:46:51 +0530

    Resume link update

--- src/components/resume/ResumeFrame.tsx
+++ src/components/resume/ResumeFrame.tsx
@@ -4,7 +4,7 @@ export default function ResumeFrame() {
   return (
     <>
       <iframe
-        src="https://drive.google.com/file/d/12DWDqzwx9Te4U1x9eAy8jOT-oIUDg6ZV/preview"
+        src="https://drive.google.com/file/d/1NrjP_JNPHvAzVeU1bw4o3Dt55yqBDO7z/preview"
         width="100%"
         height="1000"
         className="h-screen"
